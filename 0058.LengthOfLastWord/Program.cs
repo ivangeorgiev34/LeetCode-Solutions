49e5224133465c7c74b687f484466091bfc7e557
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-02-07 18:09:51 +0200

    Add solution to 0058.LengthOfLastWord problem

---
+++ 0058.LengthOfLastWord/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _0058.LengthOfLastWord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("Hello World");
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+            var lastWordStartIndex = 0;
+            var lastWordEndIndex = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ' && lastWordEndIndex == 0)
+                {
+                    lastWordEndIndex = i;
+                    continue;
+                }
+
+                if (s[i] == ' ' && lastWordEndIndex != 0)
+                {
+                    lastWordStartIndex = i + 1;
+                    break;
+                }
+            }
+
+            return lastWordEndIndex - lastWordStartIndex + 1;
+        }
+    }
+}

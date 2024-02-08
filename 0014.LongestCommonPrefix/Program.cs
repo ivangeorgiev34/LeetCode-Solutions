f515ed3b78a35deb6eb07fd6bbbc609982bebef7
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-02-08 14:26:01 +0200

    Add solution to 0014.LongestCommonPrefix problem

---
+++ 0014.LongestCommonPrefix/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _0014.LongestCommonPrefix
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestCommonPrefix(["flower", "flow", "flight"]));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+
+            var commonPrefix = strs[0];
+
+            var lastIndexPrefix = -1;
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                lastIndexPrefix = -1;
+
+                for (int j = 0; j < strs[i].Length; j++)
+                {
+                    if (commonPrefix.Length == j)
+                    {
+                        break;
+                    }
+
+                    if (strs[i][j] == commonPrefix[j])
+                    {
+                        lastIndexPrefix += 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                commonPrefix = strs[i].Substring(0, lastIndexPrefix + 1);
+
+            }
+
+            return commonPrefix;
+        }
+    }
+}

9743971974306acd3c8ec60df95d2141aac0f0be
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-02-23 20:36:37 +0200

    Add solution to 0028.FindTheIndexOfTheOccuranceInAString problem

---
+++ 0028.FindTheIndexOfTheFirstOccurrenceInAString/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _0028.FindTheIndexOfTheFirstOccurrenceInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int StrStr(string haystack, string needle)
+        {
+            var index = -1;
+
+            var flag = false;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (needle[needle.Length - 1] == haystack[i])
+                {
+
+                    index = i;
+
+                    for (int j = needle.Length - 1; j > 0; j++)
+                    {
+                        if (needle[j] == haystack[index])
+                        {
+                            flag = true;
+                            index -= 1;
+                        }
+                        else
+                        {
+                            flag = false;
+                            index = -1;
+                            break;
+                        }
+                    }
+
+                    if (flag)
+                    {
+                        return index;
+                    }
+                }
+            }
+
+            return index;
+            //"sadbutsad" "sad"
+        }
+    }
+}

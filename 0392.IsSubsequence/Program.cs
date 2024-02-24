88d0f4119419849e6f605ce4d65398304e10caaf
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-02-24 11:13:07 +0200

    Add solution to 0392.IsSubsequence problem

---
+++ 0392.IsSubsequence/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _0392.IsSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSubsequence("b", "abc"));
+        }
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            if (s == "")
+            {
+                return true;
+            }
+
+            var subsequenceIndex = 0;
+            var i = 0;
+
+            while (i < t.Length && subsequenceIndex < s.Length)
+            {
+                if (t[i] == s[subsequenceIndex])
+                {
+                    subsequenceIndex += 1;
+                }
+
+                i += 1;
+            }
+
+            return subsequenceIndex == s.Length;
+        }
+    }
+}

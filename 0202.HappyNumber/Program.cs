a6bba9a2f873b49cf74341cf7304f74986abb8d2
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-03-02 17:53:07 +0200

    Add solution to 0202.HappyNumber problem

---
+++ 0202.HappyNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _0202.HappyNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsHappy(2));
+        }
+
+        public static bool IsHappy(int n)
+        {
+            if (n == 1)
+            {
+                return true;
+            }
+            var hashSet = new HashSet<int>();
+
+            while (hashSet.Contains(n) == false)
+            {
+                if (n == 1)
+                {
+                    return true;
+                }
+
+                hashSet.Add(n);
+                n = Calculate(n);
+            }
+
+            return false;
+        }
+
+        public static int Calculate(int n)
+        {
+            var result = 0;
+
+            int currentNumber;
+            while (n > 0)
+            {
+                currentNumber = n % 10;
+                result += currentNumber * currentNumber;
+                n /= 10;
+            }
+
+            return result;
+        }
+    }
+}

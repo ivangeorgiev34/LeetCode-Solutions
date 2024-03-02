9a5cb638b118de3c32f61fc0ce0fe2c3ec52b39c
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-03-02 15:42:03 +0200

    Add solution to 0290.WordPattern problem

---
+++ 0290.WordPattern/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _0290.WordPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordPattern("abba", "dog cat cat dog"));
+        }
+        public static bool WordPattern(string pattern, string s)
+        {
+            var dictionary = new Dictionary<char, string>();
+            var index = 0;
+            var patternIndex = 0;
+
+            string word = "";
+
+            while (index <= s.Length && patternIndex < pattern.Length)
+            {
+                if (index == s.Length || s[index] == ' ')
+                {
+                    if (dictionary.ContainsKey(pattern[patternIndex]) == true)
+                    {
+                        //a dog
+                        //b dog
+                        if (dictionary[pattern[patternIndex]] != word)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (dictionary.ContainsValue(word) == false)
+                        {
+                            dictionary.Add(pattern[patternIndex], word);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+
+                    patternIndex += 1;
+                    word = "";
+                }
+                else
+                {
+                    word += s[index];
+                }
+
+                index += 1;
+            }
+            return patternIndex == pattern.Length && index == s.Length + 1;
+        }
+    }
+}

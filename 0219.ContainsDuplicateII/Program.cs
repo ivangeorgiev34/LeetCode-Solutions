eb320259b3a9dec7ac56e2e13eb5f9fcb9cadee3
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-03-02 18:42:21 +0200

    Add solution to 0219.ContainsDuplicateII problem

---
+++ 0219.ContainsDuplicateII/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _0219.ContainsDuplicateII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContainsNearbyDuplicate(new int[] { 1, 0, 1, 1 }, 1));
+        }
+
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var dictionary = new Dictionary<int, int>();
+            var index = 0;
+
+            while (index < nums.Length)
+            {
+                if (dictionary.ContainsKey(nums[index]) == true)
+                {
+                    if (Math.Abs(dictionary[nums[index]] - index) <= k)
+                    {
+                        return true;
+                    }
+
+                    dictionary[nums[index]] = index;
+                }
+                else
+                {
+                    dictionary.Add(nums[index], index);
+                }
+
+                index += 1;
+            }
+
+            return false;
+        }
+    }
+}

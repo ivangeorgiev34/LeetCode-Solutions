9415dcb914d5950912ad49731cafbcd14ca411fe
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-03-02 17:51:06 +0200

    Add solution to 0001.TwoSum problem

---
+++ 0001.TwoSum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _0001.TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var index = 0;
+            var dictionary = new Dictionary<int, int>();
+            var result = new int[2];
+
+            while (index < nums.Length)
+            {
+                if (dictionary.ContainsKey(target - nums[index]) == true)
+                {
+                    result[0] = dictionary[target - nums[index]];
+                    result[1] = index;
+
+                    break;
+                }
+
+                dictionary[nums[index]] = index;
+
+                index += 1;
+            }
+
+            return result;
+        }
+    }
+}

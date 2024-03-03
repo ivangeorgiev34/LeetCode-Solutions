3734cdf2ae149663b4ae6dc26d00f6ea3c7fae6f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-03-03 18:22:15 +0200

    Add solution to 0228.SummaryRanges problem

---
+++ 0228.SummaryRanges/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _0228.SummaryRanges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SummaryRanges(new int[] { 0, 1, 2, 4, 5, 7 }));
+        }
+
+        public static IList<string> SummaryRanges(int[] nums)
+        {
+            var index = 0;
+            var pointer = 0;
+            var sets = new List<string>();
+
+            while (index < nums.Length)
+            {
+                if (index < nums.Length - 1 && nums[index] + 1 == nums[index + 1])
+                {
+                    pointer = index;
+
+                    while (pointer < nums.Length)
+                    {
+                        if (pointer < nums.Length - 1 && nums[pointer] + 1 == nums[pointer + 1])
+                        {
+                            pointer += 1;
+
+                            continue;
+                        }
+
+                        break;
+                    }
+
+                    sets.Add($"{nums[index]}->{nums[pointer]}");
+                    index = pointer;
+                }
+                else
+                {
+                    sets.Add($"{nums[index]}");
+                }
+
+                index += 1;
+            }
+
+            return sets;
+        }
+    }
+}

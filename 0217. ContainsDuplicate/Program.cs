46c2f5e582d6fee96786d293981de84538b8a997
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-07-05 21:15:07 +0300

    Add solution to problem 0217.ContainsDuplicate

---
+++ 0217. ContainsDuplicate/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _0217._ContainsDuplicate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(ContainsDuplicate(new int[] { 1, 2, 3, 1 }));
+
+        }
+
+        public static bool ContainsDuplicate(int[] numbers)
+        {
+            var set = new HashSet<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (set.Contains(numbers[i]))
+                {
+                    return true;
+                }
+
+                set.Add(numbers[i]);
+            }
+
+            return false;
+        }
+    }
+}

79afe0e88214c0639dc538bfde336a2ff9874e3a
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-07-13 00:42:33 +0300

    Add solution to 0347.TopKFrequentElement problem

---
+++ 0347.TopKFrequentElements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _0347.TopKFrequentElements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = TopKFrequent(new int[] { 1, 1, 1, 3, 2, 2 }, 2);
+            Console.WriteLine();
+        }
+
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            var frequentElements = new List<int>();
+
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], 0);
+                }
+
+                dict[nums[i]] += 1;
+            }
+
+            var sortedDictionaryValues = dict.OrderByDescending(x => x.Value).ToArray();
+
+            for (int i = 0; i < k; i++)
+            {
+                frequentElements.Add(sortedDictionaryValues[i].Key);
+            }
+
+            return frequentElements.ToArray();
+        }
+    }
+}

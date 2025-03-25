38ed63d1d99c25e105e623a98014465c4370da3a
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-03-25 20:54:56 +0200

    Add solution for 0271.EncodeAndDecodeStrings

---
+++ 0271.EncodeAndDecodeStrings/0271.EncodeAndDecodeStrings/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _0271.EncodeAndDecodeStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Encode(new List<string>() { "leet", "code" });
+            var v = Decode(a);
+        }
+
+        private static string Encode(IList<string> strs)
+        {
+            var str = "";
+
+            var pointer = 0;
+            while (pointer < strs.Count)
+            {
+                str += strs[pointer].Length.ToString() + '#' + strs[pointer];
+
+                pointer += 1;
+            }
+
+            return str;
+        }
+
+        private static List<string> Decode(string s)
+        {
+            var strings = new List<string>();
+
+            var i = 0;
+
+            while (i < s.Length)
+            {
+                var j = i;
+                var length = "";
+
+                while (s[j] != '#')
+                {
+                    length += s[j];
+
+                    j += 1;
+                }
+
+                var word = "";
+                var temp = j + 1;
+                while (temp < j + 1 + int.Parse(length))
+                {
+                    word += s[temp];
+
+                    temp += 1;
+                }
+
+                strings.Add(word);
+
+                i = j + 1 + int.Parse(length);
+            }
+
+            return strings;
+        }
+    }
+}

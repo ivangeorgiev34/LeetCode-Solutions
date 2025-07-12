75f53ab9fefa8d086da531cdd70abebd6226c77e
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-07-13 00:34:14 +0300

    Add solution to 0238.ProductOfArrayExceptSelf problem

---
+++ 0238.ProductOfArrayExceptSelf/0238.ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _0238.ProductOfArrayExceptSelf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            var result = new int[nums.Length];
+
+            var prefix = 1;
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = prefix;
+                prefix *= nums[i];
+            }
+
+            var postfix = 1;
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                result[i] *= postfix;
+                postfix *= nums[i];
+            }
+
+            return result;
+        }
+
+    }
+}

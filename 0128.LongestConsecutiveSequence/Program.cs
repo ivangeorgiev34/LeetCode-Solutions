6510f14368696e8256acf284a7de217da3f13acb
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-07-14 01:41:06 +0300

    Add solution to 0128.LongestConsecutiveSequence

---
+++ 0128.LongestConsecutiveSequence/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _0128.LongestConsecutiveSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LongestConsecutive(new int[] { 0 });
+        }
+
+        public static int LongestConsecutive(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+
+            var longest = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.Contains(nums[i] - 1)) continue;
+
+                var length = 0;
+                while (set.Contains(length + nums[i]))
+                {
+                    length++;
+                }
+
+                if (length > longest) longest = length;
+            }
+
+            return longest;
+        }
+    }
+}

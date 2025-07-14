3684c470982c4494d07b9efde689a200a71ed20b
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-07-14 20:30:06 +0300

    Add solution to 0020.ValidParentheses problem

---
+++ 0020.ValidParentheses/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _0020.ValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsValid("()");
+        }
+
+        public static bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            var dictionary = new Dictionary<char, char>()
+            {
+                { ')','(' },
+                { ']','[' },
+                { '}','{' },
+            };
+
+            foreach (var item in s)
+            {
+                if (dictionary.ContainsKey(item))
+                {
+                    if (stack.Count > 0 && stack.Peek() == dictionary[item])
+                    {
+                        stack.Pop();
+                    }
+                    else return false;
+                }
+                else stack.Push(item);
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}

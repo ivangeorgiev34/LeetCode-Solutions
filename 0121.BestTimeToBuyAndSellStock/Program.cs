92c2213c22b308973e9769075be6ee8230b56a83
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-08-17 19:56:00 +0300

    Add solution to 0121.BestTimeToBuyAndSellStock problem

---
+++ 0121.BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _0121.BestTimeToBuyAndSellStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+
+            s.MaxProfit(new int[] { 1, 2 });
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var left = 0;
+            var right = 1;
+
+            var res = 0;
+
+            while (right < prices.Length)
+            {
+                if (prices[right] < prices[left])
+                {
+                    left = right;
+                }
+
+                if (res < prices[right] - prices[left])
+                {
+                    res = prices[right] - prices[left];
+                }
+
+                right++;
+
+            }
+
+            return res;
+        }
+    }
+}

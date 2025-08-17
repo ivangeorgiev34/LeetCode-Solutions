ebcdfbc3c13145c40df4f3b7b51e860168fbaab7
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-08-17 20:21:48 +0300

    Add solution to 0167.TwoSumInputArrayIsSorted problem

---
+++ 0167.TwoSumII-InputArrayIsSorted/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _0167.TwoSumII_InputArrayIsSorted;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+    }
+
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        var index = 0;
+        var result = new int[2];
+
+        while (index < numbers.Length)
+        {
+
+        }
+
+        return result;
+    }
+}

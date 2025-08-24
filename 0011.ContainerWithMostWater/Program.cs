7155882b2338a394ee0ee4ab89748cf85147db31
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-08-24 20:56:25 +0300

    Add solution to 0011.ContainerWithMostWater problem

---
+++ 0011.ContainerWithMostWater/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _0011.ContainerWithMostWater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            var left = 0;
+            var right = height.Length - 1;
+
+            var max = 0;
+
+            while (left < right)
+            {
+                var curr = (right - left) * (height[left] < height[right] ? height[left] : height[right]);
+                if (curr > max)
+                {
+                    max = curr;
+                }
+
+                if (height[left] > height[right]) right--;
+                else left++;
+            }
+
+            return max;
+        }
+    }
+}
